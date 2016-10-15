700b6f2b08c8427fafacef93d7dd065ad0f90f10
nabezokodaikon <nabezoko.daikon@gmail.com>
2016-10-15 23:40:56 +0900

    Add XAUTHORITY file creation process

--- start.sh
+++ start.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
+XSOCK=/tmp/.X11-unix
+XAUTH=/tmp/.1password-docker.xauth
+touch ${XAUTH}
+xauth nlist ${DISPLAY} | sed -e 's/^..../ffff/' | xauth -f ${XAUTH} nmerge -
+
 docker start -i 1password
